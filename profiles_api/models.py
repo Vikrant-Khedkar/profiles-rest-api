9dd934eceef0aa0e0e621f601475067662f37372
Vikrant Khedkar <vikrantkhedkar2720@gmail.com>
2022-10-01 01:09:45 +0530

    Created and applied migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,8 +1,39 @@
 import email
+from email.headerregistry import UniqueSingleAddressHeader
 from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self,email,name,password=None):
+        """Create a new use profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self,email,passowrd):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email,name,passowrd)
+
+        user.is_superuser = True
+        user.is_staff= True
+        user.save(using=self.db)
+
+        return user        
+
+
 
 class userprofile(AbstractBaseUser,PermissionsMixin):
     """Database model for user in the system"""
